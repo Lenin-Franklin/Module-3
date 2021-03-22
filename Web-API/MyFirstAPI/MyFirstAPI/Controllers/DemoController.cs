df2f3c647b4632d73d6cc10605d0711a8d295210
santuparsi <santuparsi@gmail.com>
2021-03-22 13:29:09 +0530

    initial

---
+++ Web-API/MyFirstAPI/MyFirstAPI/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [Route("Message")]
+        public string Get()
+        {
+            return "Hello World API..";
+        }
+        [Route("Greet/{name}")]
+        public string Get(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
