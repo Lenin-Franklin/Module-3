8e8d4bec0fea69d62eed0136a779df90e0a8fcd0
santuparsi <santuparsi@gmail.com>
2021-03-25 16:24:14 +0530

    initial

---
+++ MVC-Core/HandsOnMVCDataFromFormtoActionMethods/HandsOnMVCDataFromFormtoActionMethods/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandsOnMVCDataFromFormtoActionMethods.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Validate(string Uname,string Pwd)
+        {
+            if(Uname=="Rohan" && Pwd=="12345")
+            {
+                return RedirectToAction("Welcome", new { Uname = Uname }); //redirects to Welcome Action
+            }
+            else
+            {
+                ViewBag.ErrMsg = "Invalid Credentials";
+                return View("Login");
+            }
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string Uname)
+        {
+            ViewData["un"] = Uname;
+            return View();
+        }
+    }
+}
---
+++ MVC-Core/HandsOnMVCDataFromFormtoActionMethods/HandsOnMVCDataFromFormtoActionMethods/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandsOnMVCDataFromFormtoActionMethods.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Greet()
+        {
+            // return RedirectToAction("Details");
+            //return RedirectToAction("Login", "Account");
+            // return RedirectToAction("Contact", new { email = "san@gmail.com", mobile = "3243243434" });
+            return RedirectToAction("http://www.twitter.com");
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Contact(string email,string mobile)
+        {
+            return View();
+        }
+    }
+}
